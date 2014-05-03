3c750b123b8a7741025db6622f301c50f713840d
Mike <imike@centrum.cz>
2014-05-03 14:14:07 +0200

    Placed code which detects readers connected to PC into separate class

---
+++ Libropouch/ReaderDetector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Libropouch
+{
+    class ReaderDetector
+    {
+        public MainWindow Parent;
+
+        public ReaderDetector(MainWindow parent)
+        {
+            Parent = parent;
+
+            Parent.SourceInitialized += OnSourceInitialized;
+        }
+
+        public void OnSourceInitialized(object sender, EventArgs e)
+        {
+            var windowHandle = (new WindowInteropHelper(Parent)).Handle;
+            var src = HwndSource.FromHwnd(windowHandle);
+            src.AddHook(WndProc);
+        }
+
+        private static IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Handle WM_DEVICECHANGE... 
+            if (msg == 0x219)
+            {
+                //InitHead();
+                Debug.Write(msg.ToString());
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+}
---
+++ Libropouch/UsbSync.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Security.Cryptography;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Libropouch
+{
+    class UsbSync
+    {                
+        public UsbSync(MainWindow parent)
+        {     
+           //GetFileList(); 
+            new ReaderDetector(parent);
+        }
+
+
+
+        private static void GetFileList()
+        {
+            var drive = GetDriveLetter();
+
+            if (drive != "") 
+                return;
+            
+            //MessageBox.Show("I wasn't able to detect any connected reader device, sorry.");
+            return;
+        }
+        
+        private static String GetDriveLetter() //Get the drive letter for the reader device
+        {            
+            var searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_DiskDrive");
+
+            foreach (var queryObj in searcher.Get().Cast<ManagementObject>())
+            {
+                Debug.WriteLine(Properties.Settings.Default.Language);
+
+                if (!queryObj["PNPDeviceID"].ToString().Contains(Properties.Settings.Default.UsbPnpDeviceId) && !queryObj["Model"].ToString().Contains(Properties.Settings.Default.UsbModel)) 
+                    continue;
+
+                foreach (var partition in queryObj.GetRelated("Win32_DiskPartition"))
+                {
+                    var partitionO = (ManagementObject) partition;
+
+                    foreach (var disk in partitionO.GetRelated("Win32_LogicalDisk"))
+                    {
+                        return (string) disk["name"];
+                    }
+                }
+            }  
+            
+            return "";
+        }
+        
+      
+    }
+}
