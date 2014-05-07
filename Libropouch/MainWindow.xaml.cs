9fe2fb0a9a8fa247b392ded8ff033060d4f9ef6c
Mike <imike@centrum.cz>
2014-05-08 00:22:01 +0200

    Finished info handling

--- Libropouch/MainWindow.xaml.cs
+++ Libropouch/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
@@ -22,17 +23,13 @@ namespace Libropouch
             //new UsbSync();
 
             if (Properties.Settings.Default.UsbAutoDetect)
-                new ReaderDetector(this); //Start reader detection which automatically triggers UsbSync when reader is connected to the pc
-
-           
-            
+                new ReaderDetector(this); //Start reader detection which automatically triggers UsbSync when reader is connected to the pc                      
         }
 
-        private static readonly Queue<Tuple<string, byte, ushort>> InfoQueue = new Queue<Tuple<string, byte, ushort>>();
-        private static DateTime _infoLastShown;
-        private static bool _infoBoxVisible = false;
+        private static readonly Queue<Tuple<string, byte>> InfoQueue = new Queue<Tuple<string, byte>>();        
+        private static bool _infoBoxVisible;
 
-        public static void Info(string text, byte type = 0, ushort timer = 5)
+        public static void Info(string text, byte type = 0)
         {
             
             if (Properties.Settings.Default.UseInfoBanner == false)
@@ -42,39 +39,40 @@ namespace Libropouch
             }
 
             if (text != "")            
-                InfoQueue.Enqueue(Tuple.Create(text, type, timer)); //Add info into the info queue                            
-
-            
-
-            var border = (Border)LogicalTreeHelper.GetParent(MW.InfoBox);
-            Debug.WriteLine(border.Visibility + text);
+                InfoQueue.Enqueue(Tuple.Create(text, type)); //Add info into the info queue                                                              
 
             if (_infoBoxVisible || InfoQueue.Count == 0) //Proceed only if infobox isn't currently displayed and if the info queue isn't empty
                 return;            
-
             
             
-            var info = InfoQueue.Dequeue(); //item1 = text, item2 = type, item3 = timer
-      
-            MW.InfoBox.Text = info.Item1;                
+            var info = InfoQueue.Dequeue(); //item1 = text, item2 = type
+            text = info.Item1;
+            type = info.Item2;
+            var delay = (int) (text.Length * 0.09); //How long will be the infobox displayed, based in the text length, 1 letter = 0.09 sec
 
-            if (info.Item2 == 0) //Change colors of the infobox depending on the type of info being displayed (normal info, or error)
+            MW.InfoBox.Text = text;
+            
+            var border = (Border)LogicalTreeHelper.GetParent(MW.InfoBox);
+
+            if (type == 0) //Change colors of the infobox depending on the type of info being displayed (normal info, or error)
                 border.Style = (Style)MW.FindResource("InfoBoxOkBg");
             else
                 border.Style = (Style)MW.FindResource("InfoBoxErrorBg");
-            lock (new object())
-            {
-                border.Visibility = Visibility.Visible;
-                Debug.WriteLine("allowed to continue" + MW.InfoBoxBorder.Visibility + border.Visibility);
-            }
+            
             _infoBoxVisible = true;
-
                                 
             var sb = (Storyboard) MW.FindResource("InfoDissolve");
-            Storyboard.SetTarget(sb, border);
-            sb.Begin();                         
 
-            _infoLastShown = DateTime.Now;
+            foreach (var animation in sb.Children.OfType<DoubleAnimation>().Where(animation => animation.Name == "OutOpacity"))
+                animation.BeginTime = new TimeSpan(0, 0, delay);
+
+            foreach (var keyFrame in sb.Children.OfType<ObjectAnimationUsingKeyFrames>().Where(oaukf => oaukf.Name == "OutVisibility").SelectMany(oaukf => oaukf.KeyFrames.Cast<DiscreteObjectKeyFrame>()))
+            {
+                keyFrame.KeyTime = new TimeSpan(0, 0, delay + 2);
+            }
+
+            Storyboard.SetTarget(sb, border);
+            sb.Begin();                                     
         }
 
         private void BookGrid_OnLoaded(object sender, RoutedEventArgs e)
